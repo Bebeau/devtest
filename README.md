42f977b4713e0268d84b79356f8d7e99f50d7603
Kyle Bebeau <kylebebeau@tikis-MacBook-Pro.local>
2018-06-15 12:23:03 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Smith & Crown Developer Test
+
+This is a developer test that I've taken as a skills assessment for Smith & Crown.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+Make sure you have Nodejs on your machine.
+
+* [Nodejs](https://nodejs.org/en/download/)
+
+### Installing
+
+Installation is easy for this project. Simply clone the repo to your local machine. Navigate to the working folder in command line.
+
+Run this command to install dependent node modules.
+
+```
+NPM install
+```
+
+Then, simply start the app.
+```
+NPM start
+```
+
+## Deployment
+
+In order to deploy a live build, run...
+
+```
+NPM run-script build
+```
+Then, move the contents of the build folder output to the live server.
+
+## Authors
+
+* **Kyle Bebeau** - [The INiT Group](http://theinitgroup.com)
+
+## Acknowledgments
+
+* Hats off to [xuopled](https://github.com/xuopled) for the use of [react-svg-line-chart](https://github.com/xuopled). We used it to display the 30 day line graphs of pricing as a design element.
---
+++ src/App.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import './assets/sass/styles.css';
+import LineChart from 'react-svg-line-chart';
+
+// define crypto vars
+var data = [
+  {
+    repo: 'bitcoin/bitcoin',
+    abbr: 'BTC'
+  },
+  {
+    repo: 'ethereum/go-ethereum',
+    abbr: 'ETH'
+  },
+  {
+    repo: 'ripple/rippled',
+    abbr: 'XRP'
+  },
+  {
+    repo: 'bitcoincashorg/spec',
+    abbr: 'BCH'
+  },
+  {
+    repo: 'EOSIO/eos',
+    abbr: 'EOS'
+  },
+  {
+    repo: 'litecoin-project/litecoin',
+    abbr: 'LTC'
+  }
+];
+
+class Repos extends Component {
+  constructor(props) {
+    super(props);
+
+    // Set active class
+    this.addActiveClass = this.addActiveClass.bind(this);
+    this.state = {
+      active: false
+    };
+
+    // Pull repo info from GitHub API and set states
+    fetch('https://api.github.com/repos/'+this.props.repo+'?access_token=598bee7f70b2619b13d0fea9c7a5732189538a90')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      console.log(data);
+      this.setState({
+        name: data.name,
+        desc: data.description,
+        image: data.organization ? (data.organization.avatar_url) : null,
+        language: data.language ? (data.language) : "NA",
+        issues: data.open_issues_count.toLocaleString(data.open_issues_count, { minimumFractionDigits: 0 }),
+        forks: data.forks_count.toLocaleString(data.forks_count, { minimumFractionDigits: 0 }),
+        homepage: data.homepage,
+      });
+    });
+
+    // retrieve close prices of each crypto for the past 30 days
+    fetch('https://min-api.cryptocompare.com/data/histoday?fsym='+this.props.abbr+'&tsym=USD&limit=30')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      var points = [];
+      for (let i = 0; i < 30; i++) {
+        points.push( {x: i, y: data.Data[i].close })
+      }
+      this.setState({
+        points: points
+      });
+    });
+
+  }
+
+  addActiveClass() {
+    const currentState = this.state.active;
+    this.setState({ active: !currentState });
+  }
+
+  render(points) {
+    return( 
+      <div className={this.state.active ? 'item open' : 'item'} onClick={this.addActiveClass}>
+        
+        <section className="card">
+
+          <LineChart
+            data={this.state.points}
+            axisVisible={false}
+            gridVisible={false}
+            labelsVisible={false}
+            labelsCountY={1}
+            pathColor="rgba(255,255,255,0.25)"
+            pathVisible={true}
+            pathSmoothing={0.1}
+            pointsVisible={false}
+          />
+
+          <div className="cardCopy">
+
+            <article className="intro">
+              {this.state.image ? (
+                  <img src={this.state.image} alt="" className="image" />
+                ) : (<span className="defaultImage"></span>) 
+              }
+              <div>
+                <h3 className="name">{this.state.name}</h3>
+                <p className="desc">{this.state.desc}</p>
+              </div>
+            </article>
+
+            <a className="homepage" href={this.state.homepage} target="_Blank">View Website</a>
+            
+            <div className="metrics">
+              <article>
+                {this.state.language}
+                <span>Language</span>
+              </article>
+              <article>
+                {this.state.issues}
+                <span>Open Issues</span>
+              </article>
+              <article>
+                {this.state.forks}
+                <span>Forks</span>
+              </article>
+            </div>
+
+          </div>
+
+        </section>
+      
+      </div>
+    )
+  }
+};
+
+const repoListing = () => (
+  <section id="wrap">
+    {data.map((item, index) => (
+      <Repos
+        key={index}
+        repo={item.repo}
+        abbr={item.abbr}
+      />
+    ))}
+  </section>
+);
+
+export default repoListing;
